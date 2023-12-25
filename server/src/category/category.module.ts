b59d78540c1510d975cd782a96f93c9549a48e46
Ivan Cherviakovskiy <ivancherviakovskiy@gmail.com>
2023-12-25 23:46:32 +0300

    Added category endpoint logic

--- server/src/category/category.module.ts
+++ server/src/category/category.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categories } from './entities/category.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Categories])],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
-export class CategoryModule {}
+export class CategoryModule { }
--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -1,26 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Categories } from './entities/category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+
+  constructor(
+    @InjectRepository(Categories)
+    private readonly categoriesRepository: Repository<Categories>
+  ) { }
+
+
+  async create(createCategoryDto: CreateCategoryDto, id: number) {
+    console.log(id);
+
+    const isEmpty = await this.categoriesRepository.findBy({
+      user: { id },
+      title: createCategoryDto.title
+    });
+    if (isEmpty.length)
+      throw new BadRequestException('Такая категория уже существует')
+    const category = {
+      title: createCategoryDto.title,
+      user: {
+        id
+      }
+    }
+    return await this.categoriesRepository.save(category);
   }
 
-  findAll() {
-    return `This action returns all category`;
+  async findAll(id: number) {
+    return await this.categoriesRepository.find({
+      where: {
+        user: {
+          id
+        }
+      },
+      relations: {
+        transactions: true
+      }
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(id: number) {
+    const category = await this.categoriesRepository.findOne({
+      where: { id },
+      relations: {
+        user: true,
+        transactions: true
+      }
+    });
+    if (!category)
+      throw new NotFoundException('Категория не найдена')
+    return category;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    const category = await this.categoriesRepository.findOne({
+      where: { id }
+    });
+    if (!category)
+      throw new NotFoundException('Категория не найдена');
+    return await this.categoriesRepository.update(id, updateCategoryDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async remove(id: number) {
+    const category = await this.categoriesRepository.findOne({
+      where: { id }
+    });
+    if (!category)
+      throw new NotFoundException('Категория не найдена');
+    return await this.categoriesRepository.delete(id);
   }
 }
