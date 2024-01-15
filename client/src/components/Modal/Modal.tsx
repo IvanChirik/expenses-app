9ee23f562c7ecd137693c33b9b10cfbca400abe7
Ivan Cherviakovskiy <ivancherviakovskiy@gmail.com>
2024-01-15 23:08:20 +0300

    Added modal content.

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,11 +1,20 @@
 import React, { FC } from 'react';
 import styles from './Modal.module.css';
 import { IModal } from './Modal.props';
+import Label from '../UI/Label/Label';
+import Input from '../UI/Input/Input';
 
 const Modal: FC<IModal> = ({ isOpen, onClose }) => {
     return <>
         {isOpen && <div className={styles['modal-overlay']}>
             <div className={styles['modal-content']}>
+                <form>
+                    <Label htmlFor='title'>Описание</Label>
+                    <Input id='title' placeholder='Описание расхода' />
+                    <Label htmlFor='amount'>Сумма</Label>
+                    <Input id='amount' placeholder='Введите потрченную полусенную сумму' type='number' inputMode='numeric' />
+
+                </form>
                 <button className={styles.close} onClick={() => onClose()}>&times;</button>
                 <p>Содержимое модального окна здесь</p>
             </div>
